9bd08713afa3076de5957b6ee7a91126da2d270c
abhishekkamat <96072575+abhishekkamat@users.noreply.github.com>
2021-12-22 13:21:06 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-b=0;
+ b=0;
 function myFunction(){
-    b=b+1
+    b=b+1;
     a=document.createElement("div");
-    c=document.createElement("li");
-    c.innerHTML="New Node";
     document.getElementById("mydiv").innerHTML="";
     a.id="name"+b;
-    a.innerHTML="<h1>Node Name</h1>"
+    a.innerHTML=`<h1>Node Name</h1>
+                 <button onclick="myFunction1()">New Node</button>`
 
     document.getElementById("mydiv").appendChild(a);
-    document.getElementById("name"+b).appendChild(c);
+}
+
+function myFunction1(){
+    b=b+1;
+    a=document.createElement("div");
+    a.id="name"+b;
+    a.innerHTML=`<h1>Node Name</h1>
+                 <button onclick="myFunction1()">New Node</button>`
+    document.getElementById("name1").appendChild(a);
+
 }
